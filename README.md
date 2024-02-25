eea5acb7f2eb3bddd9139003f0f688d6a74939c0
Joshua Samenfink <joshua@razuuu.de>
2024-02-26 00:22:32 +0100

    ActionBarAPI: Update to 1.20.4
    * Bump up to 1.0.4
    * changed author
    * switched to english only

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ How To (Server Admins)
 ------
 ActionBarAPI is a jar file that you can download and run just like a normal jar file.
 
-Download a copy of ActionBarAPI-1.0.3-SNAPSHOT.jar from [releases](https://github.com/SoulsMCde/ActionBarAPI/releases/download/v1.0.3/ActionBarAPI-1.0.3-SNAPSHOT.jar).
+Download a copy of ActionBarAPI-1.0.4-SNAPSHOT.jar from [releases](https://github.com/SoulsMCeu/ActionBarAPI/releases/download/v1.0.4/ActionBarAPI-1.0.4-SNAPSHOT.jar).
 
 Paste the jar directly to your plugins folder. Just like old times
 
@@ -14,8 +14,8 @@ Paste the jar directly to your plugins folder. Just like old times
  * Maven Repo:
 ```xml
 <repository>
-    <id>arrow-systems</id>
-    <url>https://mvn.arrow-systems.de/</url>
+    <id>razuuu</id>
+    <url>https://mvn.razuuu.de/</url>
 </repository>
 ```
  * Artifact Information (For >1.0.3-SNAPSHOT):
@@ -40,6 +40,6 @@ Paste the jar directly to your plugins folder. Just like old times
  ```
 How To (Compiling Jar From Source)
 ------
-To compile ActionBarAPI, you need JDK 8, maven, and an internet connection.
+To compile ActionBarAPI, you need at least JDK 8, maven, and an internet connection.
 
 Clone this repo, run `mvn clean install`
--- src/main/java/eu/soulsmc/actionbarapi/ActionBarAPI.java
+++ src/main/java/eu/soulsmc/actionbarapi/ActionBarAPI.java
@@ -26,18 +26,18 @@ public class ActionBarAPI extends JavaPlugin implements Listener {
     public static String prefix = "§8» §9SoulsMC§8.§9eu §8┃ §7";
     private static final ConsoleCommandSender console = Bukkit.getConsoleSender();
 
+    @Override
     public void onEnable() {
         plugin = this;
 
-        nmsver = Bukkit.getServer().getClass().getPackage().getName();
-        nmsver = nmsver.substring(nmsver.lastIndexOf(".") + 1);
+        nmsver = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
 
-        if (nmsver.equalsIgnoreCase("v1_8_R1") || nmsver.startsWith("v1_7_")) {
+        if (nmsver.startsWith("v1_7_") || nmsver.startsWith("v1_8_")) {
             useOldMethods = true;
         }
 
-        console.sendMessage(prefix + "Das Plugin §6" + getDescription().getName() +
-                " §7Version §a" + getDescription().getVersion() + " §7erfolgreich §a§naktiviert§7!");
+        console.sendMessage(prefix + "Plugin §6" + getDescription().getName() +
+                " §7Version §a" + getDescription().getVersion() + " §7successfully §a§nactivated§7!");
     }
 
     public static void sendActionBar(Player player, String message) {
@@ -46,43 +46,32 @@ public class ActionBarAPI extends JavaPlugin implements Listener {
         }
         ActionBarMessageEvent actionBarMessageEvent = new ActionBarMessageEvent(player, message);
         Bukkit.getPluginManager().callEvent(actionBarMessageEvent);
-        if (actionBarMessageEvent.isCancelled())
+        if (actionBarMessageEvent.isCancelled()) {
             return;
+        }
 
         try {
             Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
             Object craftPlayer = craftPlayerClass.cast(player);
             Object packet;
-            Class<?> packetPlayOutChatClass = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
-            Class<?> packetClass = Class.forName("net.minecraft.server." + nmsver + ".Packet");
+            Class<?> packetPlayOutChatClass = Class.forName("net.minecraft.server." + nmsver + ".network.protocol.game.PacketPlayOutChat");
+            Class<?> packetClass = packetPlayOutChatClass.getSuperclass();
             if (useOldMethods) {
-                Class<?> chatSerializerClass = Class.forName("net.minecraft.server." + nmsver + ".ChatSerializer");
-                Class<?> iChatBaseComponentClass = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
+                Class<?> chatSerializerClass = Class.forName("net.minecraft.util.ChatSerializer");
+                Class<?> iChatBaseComponentClass = Class.forName("net.minecraft.network.chat.IChatBaseComponent");
                 Method m3 = chatSerializerClass.getDeclaredMethod("a", String.class);
-                Object cbc = iChatBaseComponentClass.cast(m3.invoke(chatSerializerClass, "{\"text\": \"" + message + "\"}"));
-                packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, byte.class}).newInstance(cbc, (byte) 2);
+                Object cbc = m3.invoke(chatSerializerClass, "{\"text\": \"" + message + "\"}");
+                packet = packetPlayOutChatClass.getConstructor(iChatBaseComponentClass, byte.class).newInstance(cbc, (byte) 2);
             } else {
-                Class<?> chatComponentTextClass = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
-                Class<?> iChatBaseComponentClass = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-                try {
-                    Class<?> chatMessageTypeClass = Class.forName("net.minecraft.server." + nmsver + ".ChatMessageType");
-                    Object[] chatMessageTypes = chatMessageTypeClass.getEnumConstants();
-                    Object chatMessageType = null;
-                    for (Object obj : chatMessageTypes) {
-                        if (obj.toString().equals("GAME_INFO")) {
-                            chatMessageType = obj;
-                        }
-                    }
-                    Object chatCompontentText = chatComponentTextClass.getConstructor(new Class<?>[]{String.class}).newInstance(message);
-                    packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, chatMessageTypeClass}).newInstance(chatCompontentText, chatMessageType);
-                } catch (ClassNotFoundException cnfe) {
-                    Object chatCompontentText = chatComponentTextClass.getConstructor(new Class<?>[]{String.class}).newInstance(message);
-                    packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, byte.class}).newInstance(chatCompontentText, (byte) 2);
-                }
+                Class<?> chatComponentTextClass = Class.forName("net.minecraft.network.chat.ChatComponentText");
+                Class<?> iChatBaseComponentClass = Class.forName("net.minecraft.network.chat.IChatBaseComponent");
+                Object chatCompontentText = chatComponentTextClass.getConstructor(String.class).newInstance(message);
+                packet = packetPlayOutChatClass.getConstructor(iChatBaseComponentClass, byte.class).newInstance(chatCompontentText, (byte) 2);
             }
             Method craftPlayerHandleMethod = craftPlayerClass.getDeclaredMethod("getHandle");
             Object craftPlayerHandle = craftPlayerHandleMethod.invoke(craftPlayer);
-            Field playerConnectionField = craftPlayerHandle.getClass().getDeclaredField("playerConnection");
+            Field playerConnectionField = craftPlayerHandle.getClass().getDeclaredField("b");
+            playerConnectionField.setAccessible(true);
             Object playerConnection = playerConnectionField.get(craftPlayerHandle);
             Method sendPacketMethod = playerConnection.getClass().getDeclaredMethod("sendPacket", packetClass);
             sendPacketMethod.invoke(playerConnection, packet);
@@ -91,7 +80,7 @@ public class ActionBarAPI extends JavaPlugin implements Listener {
         }
     }
 
-    public static void sendActionBar(final Player player, final String message, int duration) {
+    public static void sendActionBar(Player player, String message, int duration) {
         sendActionBar(player, message);
 
         if (duration >= 0) {
@@ -119,14 +108,12 @@ public class ActionBarAPI extends JavaPlugin implements Listener {
     }
 
     public static void sendActionBarToAllPlayers(String message, int duration) {
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            sendActionBar(p, message, duration);
-        }
+        Bukkit.getOnlinePlayers().forEach(player -> sendActionBar(player, message, duration));
     }
 
     @Override
     public void onDisable() {
-        console.sendMessage(prefix + "Das Plugin §6" + getDescription().getName() +
-                " §7Version §a" + getDescription().getVersion() + " §7erfolgreich §c§ndeaktiviert§7!");
+        console.sendMessage(prefix + "Plugin §6" + getDescription().getName() +
+                " §7Version §a" + getDescription().getVersion() + " §7successfully §c§ndeactivated§7!");
     }
 }
